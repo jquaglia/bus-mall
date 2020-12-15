c1d49946dc64a18d67ed8a16165e948184b6c597
stubtail42 <jqugali@gmail.com>
2020-12-15 10:42:22 -0800

    cleaned code up a bit

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 var maxClicksAllowed = 25;
 var actualClicks = 0;
 var allProducts = [];
+var renderQueue = [];
 
 // get ids from the DOM
 var myContainer = document.getElementById('container');
@@ -12,17 +13,16 @@ var imageTwoElement = document.getElementById('image-two');
 var imageThreeElement = document.getElementById('image-three');
 var resultsList = document.getElementById('results');
 var myButton = document.getElementById('button');
+var hiddenSection = document.getElementById('hidden-results');
 
 // product constructor
-function Product(productName, src) {
+function Product(productName, src = 'jpg') {
   this.name = productName;
   this.src = `img/${productName}.${src}`;
   this.views = 0;
   this.votes = 0;
   allProducts.push(this);
 }
-
-
 
 // DETERMINE WHICH PRODUCT IS VIEWED
 // get random index
@@ -32,49 +32,38 @@ function getRandomIndex(max) {
 
 // assign a src, alt, title to image
 function renderProducts() {
-  var productArray = [];
   // validation
-  // productArray.push(productOneIndex);
-  while (productArray.length < 3) {
+  // 1 random index, push 1 into array, get random index 2, check array for index 2, if false, move on, if true, get random index 2 again, get random index 3, check array for index 3, if true, get random index3 again, if false, move on.
+  while (renderQueue.length < 3) {
     var tempIndex = getRandomIndex(allProducts.length);
-    while (productArray.includes(tempIndex)){
+    while (renderQueue.includes(tempIndex)){
       tempIndex = getRandomIndex(allProducts.length);
     }
-    // console.log(tempIndex);
-    productArray.push(tempIndex);
-
+    renderQueue.push(tempIndex);
   }
-  // console.log(productArray);
-  // 1 random index, push 1 into array, get random index 2, check array for index 2, if false, move on, if true, get random index 2 again, get random index 3, check array for index 3, if true, get random index3 again, if false, move on.
 
-  var productOneIndex = productArray.pop();
-  // console.log('productOneIndex', productOneIndex);
-  var productTwoIndex = productArray.pop();
-  var productThreeIndex = productArray.pop();
+  var productOneIndex = renderQueue.pop();
+  var productTwoIndex = renderQueue.pop();
+  var productThreeIndex = renderQueue.pop();
 
-  // console.log(productArray);
+  productViews(imageOneElement, productOneIndex);
+  productViews(imageTwoElement, productTwoIndex);
+  productViews(imageThreeElement, productThreeIndex);
+}
 
-  imageOneElement.src = allProducts[productOneIndex].src;
-  imageOneElement.alt = allProducts[productOneIndex].name;
-  imageOneElement.title = allProducts[productOneIndex].name;
-  //log the views
-  allProducts[productOneIndex].views++;
-
-  imageTwoElement.src = allProducts[productTwoIndex].src;
-  imageTwoElement.alt = allProducts[productTwoIndex].name;
-  imageTwoElement.title = allProducts[productTwoIndex].name;
-  //log the views
-  allProducts[productTwoIndex].views++;
-
-  imageThreeElement.src = allProducts[productThreeIndex].src;
-  imageThreeElement.alt = allProducts[productThreeIndex].name;
-  imageThreeElement.title = allProducts[productThreeIndex].name;
-  //log the views
-  allProducts[productThreeIndex].views++;
+// add src, alt, title to image tag
+function productViews(imgElement, productIndex){
+  imgElement.src = allProducts[productIndex].src;
+  imgElement.alt = allProducts[productIndex].name;
+  imgElement.title = allProducts[productIndex].name;
+  allProducts[productIndex].views++;
 }
 
 // event handler
 function handleClick(event) {
+  // if (){
+  //   alert('Please click on the images')
+  // } else actualClicks++;
   actualClicks++;
   var clickedProduct = event.target.title;
   for (var i = 0; i < allProducts.length; i++) {
@@ -82,7 +71,7 @@ function handleClick(event) {
       allProducts[i].votes++;
     }
   }
-  // reassign image src properties - call that function again?
+  // reassign image src properties - call that function again
   renderProducts();
 
   // validation for when we hit our max clicks
@@ -97,37 +86,36 @@ function handleClick(event) {
 
 function handlePush(event){
   var clickedButton = event.target.button;
+  hiddenSection.style.display = 'block';
   for (var j = 0; j < allProducts.length; j++) {
     var liElement = document.createElement('li');
     liElement.textContent = `${allProducts[j].name} was viewed ${allProducts[j].views} times and clicked ${allProducts[j].votes} times`;
     resultsList.appendChild(liElement);
-    if (clickedButton === true){
-      document.removeEventListener('click', handlePush);
-    }
   }
+  document.removeEventListener('click', handlePush);
 }
 
 //Instantiations
-new Product('bag', 'jpg');
-new Product('banana', 'jpg');
-new Product('bathroom', 'jpg');
-new Product('boots', 'jpg');
-new Product('breakfast', 'jpg');
-new Product('bubblegum', 'jpg');
-new Product('chair', 'jpg');
-new Product('cthulhu', 'jpg');
-new Product('dog-duck', 'jpg');
-new Product('dragon', 'jpg');
-new Product('pen', 'jpg');
-new Product('pet-sweep', 'jpg');
-new Product('scissors', 'jpg');
-new Product('shark', 'jpg');
+new Product('bag');
+new Product('banana');
+new Product('bathroom');
+new Product('boots');
+new Product('breakfast');
+new Product('bubblegum');
+new Product('chair');
+new Product('cthulhu');
+new Product('dog-duck');
+new Product('dragon');
+new Product('pen');
+new Product('pet-sweep');
+new Product('scissors');
+new Product('shark');
 new Product('sweep', 'png');
-new Product('tauntaun', 'jpg');
-new Product('unicorn', 'jpg');
+new Product('tauntaun');
+new Product('unicorn');
 new Product('usb', 'gif');
-new Product('water-can', 'jpg');
-new Product('wine-glass', 'jpg');
+new Product('water-can');
+new Product('wine-glass');
 
 // executable code
 renderProducts();
